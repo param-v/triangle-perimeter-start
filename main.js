9038d7ef4e6062b33904c9d2b9d31bfcb75f20bd
param-v <112659738+param-v@users.noreply.github.com>
2022-09-15 14:47:30 -0600

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // Triangle Perimeter Assignment Start Code
+let xA = +document.getElementById("xA").value;
+let xB = +document.getElementById("xB").value;
+let xC = +document.getElementById("xC").value;
+let yA = +document.getElementById("yA").value;
+let yB = +document.getElementById("yB").value;
+let yC = +document.getElementById("yC").value;
